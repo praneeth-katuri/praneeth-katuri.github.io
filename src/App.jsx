f6e387c3862b95b80d054db9991514a96eb0c6f7
praneeth-katuri <praneethkaturi@outlook.com>
2025-01-05 22:40:00 +0530

    refactor: Update App.jsx with components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import ProjectsSection from "./components/Projects";
+import Contact from "./components/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <ProjectsSection />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
