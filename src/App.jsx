dcf4a13bcc8b285db94bb6ae3deae4b0265c469a
Praneeth Katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-07-05 00:14:18 +0530

    chore: add web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import About from "./components/About";
 import ProjectsSection from "./components/Projects";
 import Contact from "./components/Contact";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <div id="about">
-        <About />
+    <>
+      <div className="min-h-screen">
+        <Navbar />
+        <Hero />
+        <div id="about">
+          <About />
+        </div>
+        <div id="projects">
+          <ProjectsSection />
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
       </div>
-      <div id="projects">
-        <ProjectsSection />
-      </div>
-      <div id="contact">
-        <Contact />
-      </div>
-    </div>
+      <Analytics />
+    </>
   );
 }
 
